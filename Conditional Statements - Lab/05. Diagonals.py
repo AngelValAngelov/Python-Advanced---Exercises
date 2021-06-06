72e7c3e4d48550dbe1011792d6c8e96aa1a3ae21
Angel Angelov <64867029+AngelValAngelov@users.noreply.github.com>
2021-06-06 23:33:58 +0300

    Delete 05. Diagonals.py

--- Conditional Statements - Lab/05. Diagonals.py
+++
@@ -1,8 +0,0 @@
-rows = int(input())
-matrix = [[int(x) for x in input().split(", ")] for x in range(rows)]
-
-first = [matrix[x][x] for x in range(rows)]
-second = [matrix[x][-x - 1] for x in range(rows)]
-
-print(f"First diagonal: {', '.join(str(x) for x in first)}. Sum: {sum(first)}")
-print(f"Second diagonal: {', '.join(str(x) for x in second)}. Sum: {sum(second)}")
