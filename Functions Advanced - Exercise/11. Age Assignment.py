81b6be93032bf8523d7d89460e68faf2e3ac194a
Angel Angelov <64867029+AngelValAngelov@users.noreply.github.com>
2021-07-05 23:47:25 +0300

    Add files via upload

---
+++ Functions Advanced - Exercise/11. Age Assignment.py
@@ -0,0 +1,10 @@
+def age_assignment(*args, **kwargs):
+    names = {}
+    for name in args:
+        names[name] = kwargs[name[0]]
+
+    return names
+
+
+print(age_assignment("Peter", "George", G=26, P=19))
+print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
