4ed24c500c630673c5fdf0acff160bc537a232e5
Angel Angelov <64867029+AngelValAngelov@users.noreply.github.com>
2021-07-11 23:11:28 +0300

    Add files via upload

---
+++ Functions Advanced - Lab/04. Operate.py
@@ -0,0 +1,17 @@
+from functools import reduce
+
+
+def operate(sign, *args):
+    if sign == '+':
+        return reduce(lambda a, b: a + b, args)
+    if sign == '-':
+        return reduce(lambda a, b: a - b, args)
+    if sign == '*':
+        return reduce(lambda a, b: a * b, args)
+    if sign == '/':
+        return reduce(lambda a, b: a / b, args)
+
+
+print(operate("+", 1, 2, 3))
+print(operate("*", 3, 4))
+
